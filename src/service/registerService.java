6ab11d9c3a26b490ac29d27c06c84a2ae08a0608
mengggggggg <2107evan@gmail.com>
2024-03-02 13:17:46 +0800

    Update registerService.java

--- src/service/registerService.java
+++ src/service/registerService.java
@@ -3,7 +3,7 @@ package service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repository.registerRepository;
-import model.User;
+import model.userModel;
 
 @Service
 public class registerService {
@@ -11,6 +11,19 @@ public class registerService {
     @Autowired
     private registerRepository registerRepository;
 
-    // 在这里实现注册相关的业务逻辑方法，例如验证用户信息、新增用户等
+    private userRepository userRepository;
+
+    public boolean registerUser(userModel user) {
+       
+        userModel existingUser = userRepository.findByPhoneNumber(user.getPhoneNumber());
+        if (existingUser != null) {
+            
+            return false;
+        } else {
+           
+            userRepository.save(user);
+            return true;
+        }
+    }
 
 }
