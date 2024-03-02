579e8815c5a38d129fa48f9049b64e4d71a2e0b9
mengggggggg <2107evan@gmail.com>
2024-03-02 13:32:48 +0800

    Update logincontroller.java

--- src/controller/logincontroller.java
+++ src/controller/logincontroller.java
@@ -28,10 +28,10 @@ public class LoginController {
     public String login(@RequestParam String phoneNumber, @RequestParam String password, Model model) {
         boolean loginSuccess = userService.loginUser(phoneNumber, password);
         if (loginSuccess) {
-            // 登录成功，重定向到首页或其他页面
+            
             return "redirect:/home";
         } else {
-            // 登录失败，返回登录页面，并在页面显示错误信息
+            
             model.addAttribute("error", "Invalid credentials. Please try again.");
             return "login"; // 返回登录页面
         }
